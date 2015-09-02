b6a23f972bb087f90f50d90a3925e3da0a18409d
Andrew Waite <andrewwaite@silktide.com>
2015-09-02 12:12:57 +0100

    Fix double quote causing lint error.

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ export function setScriptLoadFunction(fn) {
 }
 
 function loadExternalScript(url) {
-  if (typeof injectedScriptLoadFunction === "function") {
+  if (typeof injectedScriptLoadFunction === 'function') {
     injectedScriptLoadFunction(url);
     return;
   }
